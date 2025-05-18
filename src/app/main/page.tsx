7359085d9da471623854f80bcb9f85ed0270eda5
mke <for.office.gthb@gmail.com>
2025-05-19 01:30:59 +0500

    Added MainPage component with ProductCard display and changed styles in globals.css to improve appearance and adaptability of input elements

---
+++ src/app/main/page.tsx
@@ -0,0 +1,11 @@
+import ProductCard from "@/entites/product/ui/ProductCard";
+
+export default function MainPage() {
+  return (
+    <div className="space-y-2">
+      <main>
+        <ProductCard />
+      </main>
+    </div>
+  );
+}
