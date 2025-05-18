48dfc744228938da0199e1e3cafc9b65f137ec08
mke <for.office.gthb@gmail.com>
2025-05-18 19:49:32 +0500

    Added MainPage component with display of product card

---
+++ src/app/main/MainPage.tsx
@@ -0,0 +1,9 @@
+import ProductCard from "@/entites/product/ui/ProductCard";
+
+export default function MainPage() {
+  return (
+    <main className="w-full grid grid-cols-3 gap-4">
+      <ProductCard />
+    </main>
+  );
+}
